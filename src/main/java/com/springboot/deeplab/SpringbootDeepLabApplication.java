475531b46728cf46e31596b96f40385c6411c41c
simGPT <simGPT02@gmail.com>
2026-02-19 13:39:44 +0900

    :tada:Start: springboot-deep-lab 프로젝트 시작

---
+++ src/main/java/com/springboot/deeplab/SpringbootDeepLabApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.deeplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeepLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeepLabApplication.class, args);
+	}
+
+}
